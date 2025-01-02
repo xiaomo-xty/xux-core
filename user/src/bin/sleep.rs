7087ff6e742716034dacadb0346533043453761d
littlesuns <xiaomonb@gmail.com>
2025-01-02 09:11:06 +0800

    task os

--- user/src/bin/sleep.rs
+++ user/src/bin/sleep.rs
@@ -8,6 +8,7 @@ fn main() -> i32 {
     let current_timer = get_time();
     let wait_for = current_timer + 3000;
     while get_time() < wait_for {
+        println!("prepare yield");
         yield_();
     }
     println!("Test sleep OK!");
